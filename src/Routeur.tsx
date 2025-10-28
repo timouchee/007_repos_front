a59ca519f392e87c6cde0545148b2af89d32d038
timeo <timeo@gmail.com>
2025-10-28 22:56:49 +0100

    adminPage

--- src/Routeur.tsx
+++ src/Routeur.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home.tsx";
-import Game from "./pages/Game.tsx"
+import Game from "./pages/Game.tsx";
+import Rules from "./pages/Rules.tsx";
+import AdminPage from "./pages/AdminPage.tsx";
 
 export default function Routeur() {
     return (
@@ -15,6 +17,8 @@ export default function Routeur() {
                 {/* <Route path="/" element={<h1>Accueil</h1>} /> */}
                 <Route path="/" element={<Home />} />
                 <Route path="/lobby" element={<Home />} />
+                <Route path="/rules" element={<Rules />} />
+                <Route path="/adminPage" element={<AdminPage />} />
                 <Route path="/game" element={<Home />} />
                 <Route path="/game/:gameId" element={<Game />} />
             </Routes>
